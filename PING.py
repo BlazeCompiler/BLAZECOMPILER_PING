ca7aaea67680779a0aa94e52c45ff59725ecd178
BLAZE_CODING_HUB <blazecodinghub@gmail.com>
2025-03-07 13:59:43 +0530

    Update PING.py

--- PING.py
+++ PING.py
@@ -3,19 +3,8 @@ import time
 
 # Updated Render service URLs
 urls = [
-    "https://blaze-python-compiler-uun1.onrender.com",
-    "https://blaze-java-compiler-d9qx.onrender.com",
-    "https://blaze-swift-compiler-js2c.onrender.com",
-    "https://blaze-golang-compiler-u45o.onrender.com",
-    "https://blaze-csharp-compiler-4wfe.onrender.com",
-    "https://blaze-ruby-compiler-mgwh.onrender.com",
-    "https://blazecompiler-r-5g3t.onrender.com",
-    "https://blazecompiler-rust-ohp8.onrender.com",
-    "https://blazecompiler-typescript-px8u.onrender.com",
-    "https://blazecompiler-javascript-g8to.onrender.com",
-    "https://blazecompiler-c-8eeb.onrender.com",
-    "https://blazecompiler-cpp-0z64.onrender.com",
-    "https://blazecompiler-php-pmzi.onrender.com"
+    "https://blazecompiler-python-m44t.onrender.com",
+    "https://blazecompiler-cpp-v236.onrender.com"
 ]
 
 while True:
